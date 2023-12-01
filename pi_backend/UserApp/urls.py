dcd219001e66b90fb8a96beb471c79497ddb7a29
edwin-tarazona <80715680+Edwin-DEV-py@users.noreply.github.com>
2023-12-01 02:55:13 -0500

    urls y view de registro de usuario y login

--- pi_backend/UserApp/urls.py
+++ pi_backend/UserApp/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import *
 
 urlpatterns = [
-    
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Obtener token de acceso
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Renovar token de acceso
+    path('register/',RegisterView.as_view(),name='register'), #vista para crear usuarios
 ]
--- pi_backend/UserApp/views.py
+++ pi_backend/UserApp/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from django.shortcuts import render,redirect
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.parsers import FileUploadParser, MultiPartParser, FormParser
+from django.db import IntegrityError
+from django.conf import settings
+from .serializers import *
+from .models import User
 
-# Create your views here.
+
+
+#registro de usuario en la API   
+class RegisterView(APIView):
+    
+    #parsear la clase para poder obtener las imagenes
+    parser_classes = (MultiPartParser, FormParser)
+    def post(self,request,*args,**kwargs):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = User.objects.create_user(**serializer.validated_data)
+            user.save()
+            
+            return Response({'message':'Usuario Registrado, verifica tu cuenta'}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
