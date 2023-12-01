e4267cc100729ddf1dd9830a2f7c0abdc604e8b4
edwin-tarazona <80715680+Edwin-DEV-py@users.noreply.github.com>
2023-12-01 03:01:53 -0500

    token de login y sesion vista y url

--- pi_backend/UserApp/urls.py
+++ pi_backend/UserApp/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Obtener token de acceso
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Renovar token de acceso
     path('register/',RegisterView.as_view(),name='register'), #vista para crear usuarios
+    path('login/',CustomLoginTokenJWT.as_view(),name='login'), #vista para generar el token de login
+    path('sesion/',CustomSessionTokenJWT.as_view(),name='sesion'), #vista para generar el token de sesion
 ]
--- pi_backend/UserApp/views.py
+++ pi_backend/UserApp/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.tokens import AccessToken
+from datetime import timedelta
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.parsers import FileUploadParser, MultiPartParser, FormParser
 from django.db import IntegrityError
@@ -27,5 +29,27 @@ class RegisterView(APIView):
             return Response({'message':'Usuario Registrado, verifica tu cuenta'}, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#token de login para ingreso con huella
+class CustomLoginTokenJWT(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        token = AccessToken.for_user(request.user)
+        token.set_exp(lifetime=timedelta(days=365))
+        
+        return Response({'login_token': str(token)})
+
+#token de sesion al ingresar con huella
+class CustomSessionTokenJWT(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        token = AccessToken.for_user(request.user)
+        token.set_exp(lifetime=timedelta(days=1))
+        
+        return Response({'sesion_token': str(token)})
 
 
